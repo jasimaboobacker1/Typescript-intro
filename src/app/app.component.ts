840b7326a2526a1c8a8bcaf1c70001dd9400f44a
unknown <jasimaboobacker1>
2024-03-14 15:55:54 +0530

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'typescript-intro';
+  constructor() {
+    // const
+    const numbers: number[] = [1, 2, 3, 4, 5];
+    console.log("Numbers:", numbers);
+
+    numbers[5] = 10; 
+    console.log('Updated Numbers:', numbers);
+
+    // let
+    let sum: number = 0;
+
+    if (true) {
+      for (let i = 0; i < numbers.length; i++) {
+          sum += numbers[i];
+      }
+      console.log("Sumbefore:", sum);
+    } 
+
+    if (true) {
+      let sum: number = 10; 
+      for (let i = 0; i < numbers.length; i++) {
+          sum += numbers[i];
+      }
+      console.log("Sumafter:", sum);
+    } 
+
+    // var
+    function exampleforVar() {
+      var message: string = "Hello, world!";
+      console.log("Message:", message);
+    }
+    exampleforVar();
+  }
 }
